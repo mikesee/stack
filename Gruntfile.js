6ee0ee19b3d22e590f53a2046283ebd1c6b1ab09
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-09-06 14:07:38 -0500

    lili

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,8 @@ module.exports = function(grunt) {
       csslint: "csslint .",
       scss_lint: "scss-lint .",
       tidy: "find . -type d -name node_modules -prune -o -type f -name '*.html' -print -exec tidy -qe {} \\; 2>&1 | grep -v 'nav>'",
-      aspelllint: "bundle exec aspelllint ."
+      aspelllint: "bundle exec aspelllint .",
+      lili: "bundle exec lili ."
 		}
   });
 
@@ -19,13 +20,21 @@ module.exports = function(grunt) {
     "exec:jshint",
     "exec:csslint",
     "exec:scss_lint",
-    "exec:tidy"
+    "exec:tidy",
+    "exec:lili"
   ]);
 
-  grunt.registerTask("lint", ["exec:jshint", "exec:csslint", "exec:tidy"]);
+  grunt.registerTask("lint", [
+    "exec:jshint",
+    "exec:csslint",
+    "exec:tidy",
+    "exec:lili"
+  ]);
+
   grunt.registerTask("jshint", ["exec:jshint"]);
   grunt.registerTask("csslint", ["exec:csslint"]);
   grunt.registerTask("scss_lint", ["exec:scss_lint"]),
   grunt.registerTask("tidy", ["exec:tidy"]);
   grunt.registerTask("aspelllint", ["exec:aspelllint"]);
+  grunt.registerTask("lili", ["exec:lili"]);
 };
